6f023e13631b0b13f30e23599787604ebd71fab9
matos12345 <112102225+matos12345@users.noreply.github.com>
2024-11-05 14:10:30 -0300

    salve12

--- grafite/informaçoesGlobais.js
+++ grafite/informaçoesGlobais.js
@@ -1,2 +1,9 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json&#39'
-console.log
+
+async function visualizarInformacoesGlobais(params) {
+    const res = await fetch (url);
+    const dados = await res.json ()
+    console.log (dados);
+    
+    }
+    visualizarInformacoesGlobais ()
